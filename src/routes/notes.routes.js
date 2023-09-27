1062fb310aecc614d3ef42943dea8dfb3ddb12c8
Vitor <vitorserta@gmail.com>
2023-09-27 18:32:45 -0300

    Back-end Rocketnotes done

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+
+const NotesController = require("../controllers/NotesControllers")
+
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+
+
+const notesRoutes = Router()
+
+
+const notesController = new NotesController()
+
+notesRoutes.use(ensureAuthenticated)
+
+notesRoutes.post("/", notesController.create)
+notesRoutes.get("/:id", notesController.show)
+notesRoutes.delete("/:id", notesController.delete)
+notesRoutes.get("/", notesController.index)
+
+module.exports = notesRoutes;
